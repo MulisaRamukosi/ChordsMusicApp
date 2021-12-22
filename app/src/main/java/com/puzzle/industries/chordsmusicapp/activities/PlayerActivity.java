4b79c14d06c3fe4e6cf9cf64e00ee1465f47be89
Mulisa <mulisa0998@gmail.com>
2021-12-23 00:13:39 +0200

    implemented and fixed PlayerActivity functionality

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/activities/PlayerActivity.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/activities/PlayerActivity.java
@@ -1,31 +1,31 @@
 package com.puzzle.industries.chordsmusicapp.activities;
 
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.ServiceConnection;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 
 import com.bumptech.glide.Glide;
-import com.puzzle.industries.chordsmusicapp.BaseActivity;
+import com.puzzle.industries.chordsmusicapp.base.BaseActivity;
 import com.puzzle.industries.chordsmusicapp.R;
 import com.puzzle.industries.chordsmusicapp.database.entities.AlbumArtistEntity;
-import com.puzzle.industries.chordsmusicapp.database.entities.ArtistEntity;
 import com.puzzle.industries.chordsmusicapp.database.entities.TrackArtistAlbumEntity;
 import com.puzzle.industries.chordsmusicapp.databinding.ActivityPlayerBinding;
 import com.puzzle.industries.chordsmusicapp.events.SongInfoProgressEvent;
 import com.puzzle.industries.chordsmusicapp.helpers.DurationHelper;
-import com.puzzle.industries.chordsmusicapp.models.dataModels.ArtistDataStruct;
 import com.puzzle.industries.chordsmusicapp.services.IMusicPlayerService;
-import com.puzzle.industries.chordsmusicapp.services.impl.MusicPlayerService;
 import com.puzzle.industries.chordsmusicapp.utils.Constants;
+import com.puzzle.industries.chordsmusicapp.utils.RepeatMode;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class PlayerActivity extends BaseActivity {
 
@@ -38,13 +38,28 @@ public class PlayerActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         mBinding = ActivityPlayerBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
-
-        initPlayer();
+        init();
     }
 
-    private void initPlayer(){
+    private void init(){
+        initSongDetails();
+
+        //wait for music service to connect
+        final ScheduledExecutorService stp = Executors.newScheduledThreadPool(1);
+        stp.scheduleAtFixedRate(() -> runOnUiThread(() -> {
+            if (getMusicPlayerService() != null){
+                initPlayer();
+                stp.shutdown();
+            }
+        }), 0, 100, TimeUnit.MILLISECONDS);
+
+    }
+
+    private void initSongDetails(){
         final Bundle bundle = getIntent().getExtras();
+
         if (bundle != null){
+            postponeEnterTransition();
             final SongInfoProgressEvent event = bundle.getParcelable(Constants.KEY_SONG);
 
             mBinding.tvSongName.setText(event.getCurrentSong().getTitle());
@@ -53,16 +68,68 @@ public class PlayerActivity extends BaseActivity {
             mBinding.sbSongProgress.setProgress(event.getCurrProgressInMilis());
             setPlayPauseButtonState(event.isPlaying());
             Glide.with(this).load(event.getCurrentAlbum().getCover_url()).into(mBinding.ivAlbum);
+            scheduleStartPostponedTransition(mBinding.ivAlbum);
         }
+    }
 
-        mBinding.ivPlayPause.setOnClickListener(v -> {
-            boolean isPlaying = mSongInfo != null && mSongInfo.isPlaying();
-            setPlayPauseButtonState(!isPlaying);
-            mMusicPlayerService.playOrPause(mSongInfo.getCurrentSong().getId());
+    private void initPlayer(){
+        getMusicPlayerService().initPlayer(mBinding.ivPlayPause,
+                mBinding.sbSongProgress,
+                mBinding.ivNext,
+                mBinding.ivPrev);
+        initRepeatButton();
+        initShuffleButton();
+
+    }
+
+    private void initShuffleButton(){
+        setShuffleButtonState();
+        mBinding.ivShuffle.setOnClickListener(v -> {
+            final IMusicPlayerService playerService = getMusicPlayerService();
+            playerService.setShuffleModeEnabled(!playerService.isShuffleModeEnabled());
+            setShuffleButtonState();
         });
     }
 
+    private void setShuffleButtonState(){
+        if (getMusicPlayerService().isShuffleModeEnabled()){
+            mBinding.ivShuffle.setColorFilter(ContextCompat.getColor(this, R.color.secondaryDarkColor),
+                    android.graphics.PorterDuff.Mode.SRC_IN);
+        }
+        else{
+            mBinding.ivShuffle.clearColorFilter();
+        }
+    }
 
+    private void initRepeatButton() {
+        setRepeatButtonState();
+        mBinding.ivRepeat.setOnClickListener(v -> {
+            final RepeatMode repeatMode = getMusicPlayerService().getRepeatMode();
+            if (repeatMode == RepeatMode.REPEAT_OFF) getMusicPlayerService().setRepeatMode(RepeatMode.REPEAT_LIST);
+            else if (repeatMode == RepeatMode.REPEAT_LIST) getMusicPlayerService().setRepeatMode(RepeatMode.REPEAT_SONG);
+            else getMusicPlayerService().setRepeatMode(RepeatMode.REPEAT_OFF);
+            setRepeatButtonState();
+        });
+    }
+
+    private void setRepeatButtonState(){
+        final RepeatMode repeatMode = getMusicPlayerService().getRepeatMode();
+        final Drawable icon = ContextCompat.getDrawable(this,
+                repeatMode == RepeatMode.REPEAT_SONG
+                        ? R.drawable.ic_round_repeat_one_24
+                        : R.drawable.ic_round_repeat_24);
+        mBinding.ivRepeat.setImageDrawable(icon);
+        final boolean isRepeatModeOn = repeatMode != RepeatMode.REPEAT_OFF;
+
+        if (isRepeatModeOn){
+            mBinding.ivRepeat.setColorFilter(ContextCompat.getColor(this, R.color.secondaryDarkColor),
+                    android.graphics.PorterDuff.Mode.SRC_IN);
+        }
+        else{
+            mBinding.ivRepeat.clearColorFilter();
+        }
+
+    }
 
     @Override
     public void onResume() {
@@ -73,6 +140,7 @@ public class PlayerActivity extends BaseActivity {
                 @Override
                 public void onReceive(Context context, Intent intent) {
                     final SongInfoProgressEvent songInfo = intent.getParcelableExtra(Constants.KEY_MUSIC_PROGRESS);
+                    if (songInfo == null) return;
                     if (mSongInfo == null || mSongInfo.getCurrentSong().getId() != songInfo.getCurrentSong().getId()){
                         initMusicState(songInfo);
                     }
@@ -82,7 +150,7 @@ public class PlayerActivity extends BaseActivity {
                 }
             };
         }
-        registerReceiver(mMusicUpdatedReceiver, new IntentFilter(Constants.KEY_MUSIC_UPDATE));
+        registerReceiver(mMusicUpdatedReceiver, new IntentFilter(Constants.ACTION_MUSIC_PROGRESS_UPDATE));
     }
 
     private void initMusicState(SongInfoProgressEvent songInfo) {
@@ -90,7 +158,7 @@ public class PlayerActivity extends BaseActivity {
         final AlbumArtistEntity album = songInfo.getCurrentAlbum();
         mSongInfo = songInfo;
 
-        Glide.with(this).load(album.getCover_url()).into(mBinding.ivAlbum);
+        displayImageFromLink(album.getCover_url(), mBinding.ivAlbum, true);
         mBinding.tvSongName.setText(track.getTitle());
         mBinding.tvSongArtist.setText(track.getName());
         mBinding.sbSongProgress.setMax(songInfo.getSongDurationInMilis());
@@ -100,10 +168,12 @@ public class PlayerActivity extends BaseActivity {
         setPlayPauseButtonState(mSongInfo.isPlaying());
     }
 
+
     @Override
     public void onPause() {
         super.onPause();
         unregisterReceiver(mMusicUpdatedReceiver);
+        mMusicUpdatedReceiver = null;
     }
 
     private void setPlayPauseButtonState(boolean isPlaying) {
@@ -113,6 +183,9 @@ public class PlayerActivity extends BaseActivity {
     }
 
     private void updateState(SongInfoProgressEvent songInfo){
+        if (mSongInfo.isPlaying() != songInfo.isPlaying()){
+            setPlayPauseButtonState(songInfo.isPlaying());
+        }
         mSongInfo.setPlaying(songInfo.isPlaying());
         mBinding.tvCurrProgress.setText(DurationHelper.minutesSecondsToString(songInfo.getCurrProgressInMilis()));
         setSongProgress(songInfo.getCurrProgressInMilis());
