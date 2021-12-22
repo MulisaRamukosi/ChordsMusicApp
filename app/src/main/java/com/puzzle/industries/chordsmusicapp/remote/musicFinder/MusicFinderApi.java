de65e43ff005d0e32c370c06972cb465d0c953ee
Mulisa <mulisa0998@gmail.com>
2021-12-23 00:26:38 +0200

    added retryPolicy

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/remote/musicFinder/MusicFinderApi.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/remote/musicFinder/MusicFinderApi.java
@@ -1,8 +1,9 @@
 package com.puzzle.industries.chordsmusicapp.remote.musicFinder;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
+import android.net.http.SslError;
 import android.util.Log;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
@@ -21,26 +22,27 @@ import java.io.IOException;
 import java.util.Locale;
 
 
-public class MusicFinderApi extends WebViewClient {
+public class MusicFinderApi extends WebViewClient implements RetryPolicyListener{
 
     private final String[] mOffensiveWords;
     private final WebView mWebView;
-    private final Context mCtx;
     private final ScriptLoaderUtils mScriptLoader;
     private final ApiCallBack<String> mCallBack;
     private final SongDataStruct mSong;
+    private final IRetryPolicy mRetryPolicy;
 
-    private final String TAG = "MUSIC_FINDER_API";
-    private final int[] STATE_TRACK = new int[1];
+    public static final String TAG = "MUSIC_FINDER_API";
+    private final int[] STATE_TRACK = new int[]{0};
 
     @SuppressLint("SetJavaScriptEnabled")
     public MusicFinderApi(SongDataStruct songDataStruct, ApiCallBack<String> callBack) {
-        mCtx = Chords.getAppContext();
-        mOffensiveWords = mCtx.getResources().getStringArray(R.array.badWords);
+        Log.d(TAG, "about to init");
+        mOffensiveWords = Chords.getAppContext().getResources().getStringArray(R.array.badWords);
         mCallBack = callBack;
         mWebView = new WebView(Chords.getAppContext());
         mScriptLoader = ScriptLoaderUtils.getInstance();
         mSong = songDataStruct;
+        mRetryPolicy = new RetryPolicy(30, 3, this);
 
         mWebView.setWebViewClient(this);
         mWebView.setWebChromeClient(new WebChromeClient());
@@ -49,7 +51,13 @@ public class MusicFinderApi extends WebViewClient {
         mWebView.getSettings().setBlockNetworkLoads(false);
         mWebView.getSettings().setBlockNetworkImage(false);
         mWebView.getSettings().setDomStorageEnabled(true);
-        mWebView.setDownloadListener((url, userAgent, contentDisposition, mimetype, contentLength) -> callBack.onSuccess(url));
+        mWebView.setDownloadListener((url, userAgent, contentDisposition, mimetype, contentLength) -> {
+            mRetryPolicy.stopRetryPolicy();
+            callBack.onSuccess(url);
+        });
+
+        mRetryPolicy.startRetryPolicy();
+        Log.d(TAG, "init complete");
 
     }
 
@@ -64,7 +72,7 @@ public class MusicFinderApi extends WebViewClient {
     }
 
     private void sendSearchQuery() throws IOException {
-        String query = String.format(Locale.US, "%s %s", mSong.getArtist().getName(), mSong.getSongName());
+        String query = String.format(Locale.US, "%s %s audio", mSong.getArtist().getName(), mSong.getSongName());
         query = applyBadWordsFix(query);
         String searchScript = mScriptLoader.getScript(Constants.SCRIPT_SEND_QUERY);
         searchScript = String.format(searchScript, query);
@@ -74,13 +82,13 @@ public class MusicFinderApi extends WebViewClient {
     }
 
     private void openSongLink() throws IOException{
-        String openLinkScript = mScriptLoader.getScript(Constants.SCRIPT_OPEN_SONG_LINK);
+        final String openLinkScript = mScriptLoader.getScript(Constants.SCRIPT_OPEN_SONG_LINK);
         Log.d(TAG, openLinkScript);
         mWebView.evaluateJavascript(openLinkScript, value -> STATE_TRACK[0] += 1);
     }
 
     private void attemptToDownloadSong() throws IOException{
-        String attemptDownload = mScriptLoader.getScript(Constants.SCRIPT_ATTEMPT_DOWNLOAD);
+        final String attemptDownload = mScriptLoader.getScript(Constants.SCRIPT_ATTEMPT_DOWNLOAD);
         Log.d(TAG, attemptDownload);
         mWebView.evaluateJavascript(attemptDownload, value -> STATE_TRACK[0] += 1);
     }
@@ -88,24 +96,38 @@ public class MusicFinderApi extends WebViewClient {
     private void downloadSong() throws IOException{
         String downloadSongScript = mScriptLoader.getScript(Constants.SCRIPT_DOWNLOAD);
         Log.d(TAG, downloadSongScript);
-        mWebView.evaluateJavascript(downloadSongScript, value -> {
-            STATE_TRACK[0] += 1;
-        });
+        mWebView.evaluateJavascript(downloadSongScript, value -> STATE_TRACK[0] += 1);
 
     }
 
     @Override
     public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
         super.onReceivedHttpError(view, request, errorResponse);
-        mCallBack.onFailure(new Throwable(String.format("Failed to get the url of %s", mSong.getSongName())));
+        Log.d(TAG, "on Received http error");
+        stopRetryAttemptsIfFinished();
     }
+
+    @Override
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        super.onReceivedSslError(view, handler, error);
+        Log.d(TAG, "on Received ssl error");
+        stopRetryAttemptsIfFinished();
+    }
+
+    @Override
+    public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+        super.onReceivedError(view, request, error);
+        Log.d(TAG, "on Received error");
+        stopRetryAttemptsIfFinished();
+    }
+
 
     @Override
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
 
         Log.d(TAG, "Current URL: " + url);
-        int currentState = STATE_TRACK[0];
+        final int currentState = STATE_TRACK[0];
 
         try {
             switch (currentState){
@@ -115,9 +137,29 @@ public class MusicFinderApi extends WebViewClient {
                 case Constants.STATE_PRIOR_DOWNLOAD: downloadSong(); break;
             }
         }catch (IOException e){
+            mRetryPolicy.stopRetryPolicy();
             mCallBack.onFailure(e);
         }
 
     }
 
+    private void stopRetryAttemptsIfFinished(){
+        if (mRetryPolicy.allAttemptsUsed()){
+            mRetryPolicy.stopRetryPolicy();
+            mCallBack.onFailure(new Throwable(String.format("Failed to get the url of %s", mSong.getSongName())));
+        }
+    }
+
+    @Override
+    public void onRetryPolicy() {
+        STATE_TRACK[0] = 0;
+        mRetryPolicy.stopRetryPolicy();
+        mRetryPolicy.startRetryPolicy();
+        mWebView.loadUrl(Constants.WEB_SITE_BASE_URL);
+    }
+
+    @Override
+    public void retryAttemptsFinished() {
+        mCallBack.onFailure(new Throwable(String.format("Failed to get the url of %s", mSong.getSongName())));
+    }
 }
