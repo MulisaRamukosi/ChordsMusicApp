e4ff9bdbfc334ad05221abfcb5367e66e9c7ee9d
Mulisa <mulisa0998@gmail.com>
2021-12-23 00:24:01 +0200

    optimized code

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/database/dao/AlbumDao.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/database/dao/AlbumDao.java
@@ -17,16 +17,16 @@ public interface AlbumDao {
     long insert(AlbumEntity album);
 
     @Query("delete from Album where Album.id = :id")
-    void delete(int id);
+    int delete(int id);
 
     @Query("delete from Album where Album.artist_id = :id")
     void deleteAlbumsByArtistId(int id);
 
-    @Query("Select Album.id, Album.title, Album.cover_url, Artist.name from Album inner join Artist " +
+    @Query("Select Album.id, Album.title, Album.cover_url, Album.artist_id, Artist.name from Album inner join Artist " +
             "on Album.artist_id = Artist.id order by Album.title asc")
     List<AlbumArtistEntity> getAllAlbums();
 
-    @Query("Select Album.id, Album.title, Album.cover_url, Artist.name from Album inner join Artist " +
+    @Query("Select Album.id, Album.title, Album.cover_url, Album.artist_id, Artist.name from Album inner join Artist " +
             "on Album.artist_id = Artist.id where Album.artist_id = :id order by Album.title asc")
     List<AlbumArtistEntity> getArtistsAlbums(int id);
 }
