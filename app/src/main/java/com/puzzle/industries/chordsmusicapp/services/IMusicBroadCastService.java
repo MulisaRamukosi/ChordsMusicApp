8533cae00e5148f0265b9af3ad9de458e7433c85
Mulisa <mulisa0998@gmail.com>
2021-12-23 00:10:59 +0200

    removed unused files

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/services/IMusicBroadCastService.java
+++
@@ -1,9 +0,0 @@
-package com.puzzle.industries.chordsmusicapp.services;
-
-public interface IMusicBroadCastService {
-
-    void musicQueueChanged();
-    void artistQueueChanged();
-    void albumQueueChanged();
-
-}
