e9573a3f47dffa930301c94bec1d0817852edfc9
Mulisa <mulisa0998@gmail.com>
2021-12-23 00:28:17 +0200

    created media player actions to be used in musicPlayer service

---
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/utils/MediaPlayerActions.java
@@ -0,0 +1,8 @@
+package com.puzzle.industries.chordsmusicapp.utils;
+
+public interface MediaPlayerActions {
+    String ACTION_PREV = "PREV";
+    String ACTION_NEXT = "NEXT";
+    String ACTION_RESUME = "PLAY";
+    String ACTION_PAUSE = "PAUSE";
+}
