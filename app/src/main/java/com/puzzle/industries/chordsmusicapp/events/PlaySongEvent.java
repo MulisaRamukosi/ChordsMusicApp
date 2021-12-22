f41ca90e63ba232d44e4eb80e3da8d094ae91acf
Mulisa <mulisa0998@gmail.com>
2021-12-23 00:12:23 +0200

    added playlist to playSongEvent to be able to add other songs to the queue

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/events/PlaySongEvent.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/events/PlaySongEvent.java
@@ -1,10 +1,13 @@
 package com.puzzle.industries.chordsmusicapp.events;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class PlayPauseSongEvent {
+public class PlaySongEvent {
     private final int id;
+    private final List<Integer> playList;
 }
