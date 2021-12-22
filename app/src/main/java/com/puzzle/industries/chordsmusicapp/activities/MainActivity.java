bc14898837bb870029f0c4efd13e5fcc5833f886
Mulisa <mulisa0998@gmail.com>
2021-12-23 00:30:55 +0200

    code optimization

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/activities/MainActivity.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/activities/MainActivity.java
@@ -6,9 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
-import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityOptionsCompat;
@@ -18,26 +16,20 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.bumptech.glide.Glide;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.puzzle.industries.chordsmusicapp.BaseActivity;
+import com.puzzle.industries.chordsmusicapp.base.BaseMediaActivity;
 import com.puzzle.industries.chordsmusicapp.R;
 import com.puzzle.industries.chordsmusicapp.database.entities.AlbumArtistEntity;
 import com.puzzle.industries.chordsmusicapp.database.entities.TrackArtistAlbumEntity;
 import com.puzzle.industries.chordsmusicapp.databinding.ActivityMainBinding;
-import com.puzzle.industries.chordsmusicapp.events.PlayPauseSongEvent;
 import com.puzzle.industries.chordsmusicapp.events.SongInfoProgressEvent;
-import com.puzzle.industries.chordsmusicapp.services.impl.MusicLibraryService;
-import com.puzzle.industries.chordsmusicapp.services.impl.MusicPlayerService;
 import com.puzzle.industries.chordsmusicapp.utils.Constants;
 
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseMediaActivity {
 
     private ActivityMainBinding mBinding;
     private NavHostFragment mNavHost;
@@ -51,7 +43,6 @@ public class MainActivity extends BaseActivity {
 
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
-
         init();
 
     }
@@ -85,32 +76,7 @@ public class MainActivity extends BaseActivity {
             startActivity(i, optionsCompat.toBundle());
         });
 
-        mBinding.ivPlayPause.setOnClickListener(v -> {
-            boolean isPlaying = mSongInfo != null && mSongInfo.isPlaying();
-            if (mSongInfo != null){
-                mMusicPlayerService.playOrPause(mSongInfo.getCurrentSong().getId());
-            }
-            setPlayPauseButtonState(!isPlaying);
-        });
-
-        mBinding.sbSongProgress.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (fromUser){
-                    mMusicPlayerService.seekTo(progress);
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+       getMusicPlayerService().initMiniPlayer(mBinding.ivPlayPause, mBinding.sbSongProgress);
     }
 
     private void initNavigation(){
@@ -129,6 +95,10 @@ public class MainActivity extends BaseActivity {
                 @Override
                 public void onReceive(Context context, Intent intent) {
                     final SongInfoProgressEvent songInfo = intent.getParcelableExtra(Constants.KEY_MUSIC_PROGRESS);
+                    if (songInfo == null){
+                        hideMediaPlayer();
+                        return;
+                    }
                     if (mSongInfo == null || mSongInfo.getCurrentSong().getId() != songInfo.getCurrentSong().getId()){
                         initMusicState(songInfo);
                     }
@@ -138,7 +108,12 @@ public class MainActivity extends BaseActivity {
                 }
             };
         }
-        registerReceiver(mMusicUpdatedReceiver, new IntentFilter(Constants.KEY_MUSIC_UPDATE));
+        registerReceiver(mMusicUpdatedReceiver, new IntentFilter(Constants.ACTION_MUSIC_PROGRESS_UPDATE));
+    }
+
+    private void hideMediaPlayer(){
+        mSongInfo = null;
+        mBinding.llPlayer.setVisibility(View.GONE);
     }
 
     private void initMusicState(SongInfoProgressEvent songInfo) {
@@ -165,25 +140,7 @@ public class MainActivity extends BaseActivity {
     public void onPause() {
         super.onPause();
         unregisterReceiver(mMusicUpdatedReceiver);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        EventBus.getDefault().register(this);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        EventBus.getDefault().unregister(this);
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onPlayPauseEvent(PlayPauseSongEvent event){
-        if (mMusicPlayerService != null){
-            mMusicPlayerService.playOrPause(event.getId());
-        }
+        mMusicUpdatedReceiver = null;
     }
 
     private void updateState(SongInfoProgressEvent songInfo){
