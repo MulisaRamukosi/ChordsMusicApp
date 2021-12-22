66d16a6034eafb96a52cd79e1bd860da74a5bab6
Mulisa <mulisa0998@gmail.com>
2021-12-23 00:00:25 +0200

    implemented artist view activity

---
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/activities/ArtistViewActivity.java
@@ -0,0 +1,91 @@
+package com.puzzle.industries.chordsmusicapp.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.puzzle.industries.chordsmusicapp.base.BaseMediaActivity;
+import com.puzzle.industries.chordsmusicapp.R;
+import com.puzzle.industries.chordsmusicapp.database.entities.ArtistEntity;
+import com.puzzle.industries.chordsmusicapp.databinding.ActivityArtistViewBinding;
+import com.puzzle.industries.chordsmusicapp.fragments.ArtistAlbumFragment;
+import com.puzzle.industries.chordsmusicapp.fragments.ArtistMusicFragment;
+import com.puzzle.industries.chordsmusicapp.base.BaseVPAdapter;
+import com.puzzle.industries.chordsmusicapp.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArtistViewActivity extends BaseMediaActivity {
+
+    private ActivityArtistViewBinding mBinding;
+    private ArtistEntity mArtist;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = ActivityArtistViewBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        postponeEnterTransition();
+        init();
+    }
+
+    private void init(){
+        if (initArtist()){
+            initArtistInfo();
+            initArtistSongsSection();
+        }
+    }
+
+    private void initArtistSongsSection() {
+        final Bundle bundle = new Bundle();
+        bundle.putInt(Constants.KEY_ARTIST_ID, mArtist.getId());
+
+        final ArtistMusicFragment artistMusicFragment = new ArtistMusicFragment();
+        final ArtistAlbumFragment artistAlbumFragment = new ArtistAlbumFragment();
+
+        artistMusicFragment.setArguments(bundle);
+        artistAlbumFragment.setArguments(bundle);
+
+        final List<String> fragmentTitles = new ArrayList<>(Arrays.asList(
+                getString(R.string.songs),
+                getString(R.string.albums)
+        ));
+
+        final List<Fragment> fragmentList = new ArrayList<>(Arrays.asList(
+                artistMusicFragment,
+                artistAlbumFragment
+        ));
+
+        mBinding.vp.setAdapter(new BaseVPAdapter(this, fragmentList));
+        new TabLayoutMediator(mBinding.tl, mBinding.vp, (tab, position) -> tab.setText(fragmentTitles.get(position)))
+                .attach();
+    }
+
+    private boolean initArtist(){
+        final Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            mArtist = bundle.getParcelable(Constants.KEY_ARTIST);
+            return mArtist != null;
+        }
+        return false;
+    }
+
+    private void initArtistInfo(){
+        if (mArtist.getPicture_url() != null){
+            displayImageFromLink(mArtist.getPicture_url(), mBinding.ivArtist, true);
+        }
+        else{
+            displayImageFromDrawable(R.drawable.bg_artist, mBinding.ivArtist, true);
+        }
+
+        mBinding.tvArtistName.setText(mArtist.getName());
+    }
+
+}
