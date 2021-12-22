83054a4237f9a8c972170e218c6182a533aa0dca
Mulisa <mulisa0998@gmail.com>
2021-12-23 00:20:42 +0200

    implemented service start when the app launches

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/Chords.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/Chords.java
@@ -1,19 +1,19 @@
 package com.puzzle.industries.chordsmusicapp;
 
-import static java.lang.System.out;
-
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Handler;
 
 import androidx.room.Room;
 
 import com.puzzle.industries.chordsmusicapp.database.ChordsMusicDB;
 import com.puzzle.industries.chordsmusicapp.database.Constants;
+import com.puzzle.industries.chordsmusicapp.services.impl.MediaPlayerNotificationService;
+import com.puzzle.industries.chordsmusicapp.services.impl.MusicLibraryService;
 import com.puzzle.industries.chordsmusicapp.services.impl.MusicPlayerService;
-
-
 
 public class Chords extends Application {
 
@@ -29,6 +29,10 @@ public class Chords extends Application {
         return instance.getApplicationContext();
     }
 
+    public static Resources getAppResources(){
+        return instance.getApplicationContext().getResources();
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -36,7 +40,23 @@ public class Chords extends Application {
         instance = this;
         db = Room.databaseBuilder(this, ChordsMusicDB.class, Constants.DB_NAME).build();
         applicationHandler = new Handler(this.getApplicationContext().getMainLooper());
-        startService(new Intent(this, MusicPlayerService.class));
+        MediaPlayerNotificationService.getInstance().createNotificationChannel();
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            startForegroundService(new Intent(this, MusicLibraryService.class));
+            startForegroundService(new Intent(this, MusicPlayerService.class));
+        }
+        else{
+            startService(new Intent(this, MusicLibraryService.class));
+            startService(new Intent(this, MusicPlayerService.class));
+        }
+
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        stopService(new Intent(this, MusicPlayerService.class));
+        stopService(new Intent(this, MusicLibraryService.class));
     }
 }
