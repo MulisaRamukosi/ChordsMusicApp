e2559f0ef10a87b521a28d17e26640d8aa9450ca
Mulisa <mulisa0998@gmail.com>
2021-12-23 09:10:27 +0200

    created a base activity for media activities

---
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/base/BaseMediaActivity.java
@@ -0,0 +1,50 @@
+package com.puzzle.industries.chordsmusicapp.base;
+
+import android.content.Intent;
+
+import com.puzzle.industries.chordsmusicapp.activities.PlayerActivity;
+import com.puzzle.industries.chordsmusicapp.database.entities.TrackArtistAlbumEntity;
+import com.puzzle.industries.chordsmusicapp.events.PlaySongEvent;
+import com.puzzle.industries.chordsmusicapp.utils.Constants;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public abstract class BaseMediaActivity extends BaseActivity{
+
+    @Setter @Getter
+    private List<TrackArtistAlbumEntity> tracks;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onPlayPauseEvent(PlaySongEvent event){
+        if (getMusicPlayerService() != null){
+
+            if (getMusicPlayerService().getCurrentSong() != null && getMusicPlayerService().getCurrentSong().getId() == event.getId()){
+                final Intent i = new Intent(this, PlayerActivity.class);
+                startActivity(i);
+            }
+            else{
+                getMusicPlayerService().play(event);
+            }
+        }
+    }
+
+}
