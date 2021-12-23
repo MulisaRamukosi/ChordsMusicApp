ca47f8299d68764e3a01ccc6197164b2eacee046
Mulisa <mulisa0998@gmail.com>
2021-12-23 09:15:09 +0200

    added new callback functions

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/callbacks/DownloadProgressCallback.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/callbacks/DownloadProgressCallback.java
@@ -1,7 +1,11 @@
 package com.puzzle.industries.chordsmusicapp.callbacks;
 
+import com.puzzle.industries.chordsmusicapp.models.dataModels.SongDataStruct;
+
 public interface DownloadProgressCallback {
 
     void updateProgress(int currentProgress);
+    void downloadComplete(SongDataStruct song);
+    void downloadFailed();
 
 }
