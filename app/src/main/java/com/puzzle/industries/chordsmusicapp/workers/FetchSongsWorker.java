6c5e58157d437f833df9cb90e883271b681cc323
Mulisa <mulisa0998@gmail.com>
2021-12-23 09:57:35 +0200

    add library initialization

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/workers/FetchSongsWorker.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/workers/FetchSongsWorker.java
@@ -8,7 +8,11 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 import com.puzzle.industries.chordsmusicapp.Chords;
+import com.puzzle.industries.chordsmusicapp.database.entities.TrackArtistAlbumEntity;
 import com.puzzle.industries.chordsmusicapp.services.impl.MusicLibraryService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FetchSongsWorker extends Worker {
 
@@ -24,6 +28,8 @@ public class FetchSongsWorker extends Worker {
     }
 
     private void loadSongs(){
-        MusicLibraryService.getInstance().setMusicList(Chords.getDatabase().trackDao().getAllTracks());
+        final List<TrackArtistAlbumEntity> tracks = Chords.getDatabase().trackDao().getAllTracks();
+        MusicLibraryService.getInstance().setMusicPlaylist(tracks.stream().mapToInt(TrackArtistAlbumEntity::getId).boxed().collect(Collectors.toList()));
+        MusicLibraryService.getInstance().setMusicList(tracks);
     }
 }
