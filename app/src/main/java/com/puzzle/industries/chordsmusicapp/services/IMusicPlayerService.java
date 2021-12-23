edf4f413965a2e068d258e811f0c85660fb1b504
Mulisa <mulisa0998@gmail.com>
2021-12-23 10:00:18 +0200

    added more functions on music player service

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/services/IMusicPlayerService.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/services/IMusicPlayerService.java
@@ -1,21 +1,31 @@
 package com.puzzle.industries.chordsmusicapp.services;
 
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import com.puzzle.industries.chordsmusicapp.database.entities.AlbumArtistEntity;
+import com.puzzle.industries.chordsmusicapp.database.entities.ArtistEntity;
 import com.puzzle.industries.chordsmusicapp.database.entities.TrackArtistAlbumEntity;
-import com.puzzle.industries.chordsmusicapp.models.dataModels.SongDataStruct;
+import com.puzzle.industries.chordsmusicapp.events.PlaySongEvent;
+import com.puzzle.industries.chordsmusicapp.utils.RepeatMode;
 
 import java.util.List;
 
 public interface IMusicPlayerService {
 
     void pause();
-    void resume();
-    void playSong(int id);
-    void setPlayList(List<TrackArtistAlbumEntity> songs);
+    void requestToPlay();
+    void playSong(int id, List<Integer> playList);
     void playNextSong();
     void playPreviousSong();
+    void setRepeatMode(RepeatMode repeatMode);
+    void setShuffleModeEnabled(boolean enabled);
+    boolean isShuffleModeEnabled();
+    RepeatMode getRepeatMode();
     TrackArtistAlbumEntity getCurrentSong();
-    int getTotalDuration();
     void seekTo(int pos);
-    void playOrPause(int id);
+    void play(PlaySongEvent event);
+    void initMiniPlayer(ImageView playPause, SeekBar songProgress);
+    void initPlayer(ImageView playPause, SeekBar songProgress, ImageView nextSong, ImageView prevSong);
 
 }
