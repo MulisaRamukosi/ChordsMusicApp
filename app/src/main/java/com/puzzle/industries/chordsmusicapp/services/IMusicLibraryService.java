58b2f8d2b6d03df83843e3d39b094ca7941e7f3c
Mulisa <mulisa0998@gmail.com>
2021-12-23 10:00:01 +0200

    added more functions on music library

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/services/IMusicLibraryService.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/services/IMusicLibraryService.java
@@ -8,15 +8,32 @@ import java.util.List;
 
 public interface IMusicLibraryService {
 
+    void setMusicPlaylist(List<Integer> playlist);
     void setMusicList(List<TrackArtistAlbumEntity> songs);
     void setArtistList(List<ArtistEntity> artists);
     void setAlbumList(List<AlbumArtistEntity> albums);
-    ArtistEntity getArtistById(int id);
-    AlbumArtistEntity getAlbumById(int id);
     void addSong(TrackArtistAlbumEntity song);
     void addArtist(ArtistEntity artist);
     void addAlbum(AlbumArtistEntity album);
+    void setCurrentQueuePos(int pos);
+    void addSongToQueue(int songId);
+    void addSongsToQueue(List<Integer> songIds);
+    void setAsShuffled(boolean shuffle);
+
+    List<Integer> getPlaylist();
+    List<TrackArtistAlbumEntity> getSongs();
+    List<TrackArtistAlbumEntity> getArtistSongs(int artistId);
+    List<TrackArtistAlbumEntity> getAlbumSongs(int albumId);
+    List<ArtistEntity> getArtists();
+    List<AlbumArtistEntity> getAlbums();
+    List<AlbumArtistEntity> getArtistAlbums(int artistId);
+
+    TrackArtistAlbumEntity getSongById(int id);
+    ArtistEntity getArtistById(int id);
+    AlbumArtistEntity getAlbumById(int id);
+
     boolean removeSong(TrackArtistAlbumEntity song);
     boolean removeAlbum(AlbumArtistEntity album);
-
+    boolean removeArtist(ArtistEntity artist);
+    boolean containsSong(int songId);
 }
