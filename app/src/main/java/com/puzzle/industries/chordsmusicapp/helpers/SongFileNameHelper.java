f6db755c4a71307dd06b12e9c9fa0cbbacd445aa
Mulisa <mulisa0998@gmail.com>
2021-12-23 10:02:47 +0200

    implemented bad characters filtering

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/helpers/SongFileNameHelper.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/helpers/SongFileNameHelper.java
@@ -4,8 +4,18 @@ import com.puzzle.industries.chordsmusicapp.models.dataModels.SongDataStruct;
 
 public class SongFileNameHelper {
 
+    private static final String[] charactersToBeRemoved = new String[]{
+            "#", "<", "$", "%", ">", "!", "&", "*", "'", "{", "}", "?", "\"",
+            "/", ":", "\\", "@", "+", "`", "|", "="
+    };
+
     public static String generateSongFileName(SongDataStruct song){
-        return String.format("%s_%s_%s.mp3", song.getArtist().getName(),
-                song.getAlbum().getTitle(), song.getSongName()).replace(' ', '_');
+        String fileName = String.format("%s_%s_%s.mp3", song.getArtist().getName(),
+                song.getAlbum().getTitle(), song.getSongName())
+                .replace(' ', '_');
+        for (String c : charactersToBeRemoved){
+            fileName = fileName.replace(c, "");
+        }
+        return fileName;
     }
 }
