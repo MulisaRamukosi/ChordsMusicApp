a23529e6c282e6fdb3f43e95f4c87774a18f68f2
Mulisa <mulisa0998@gmail.com>
2022-01-17 20:02:59 +0200

    changed file name structure

--- app/src/main/java/com/puzzle/industries/chordsmusicapp/helpers/SongFileNameHelper.java
+++ app/src/main/java/com/puzzle/industries/chordsmusicapp/helpers/SongFileNameHelper.java
@@ -1,6 +1,9 @@
 package com.puzzle.industries.chordsmusicapp.helpers;
 
+import com.puzzle.industries.chordsmusicapp.models.dataModels.AlbumDataStruct;
 import com.puzzle.industries.chordsmusicapp.models.dataModels.SongDataStruct;
+
+import java.util.Locale;
 
 public class SongFileNameHelper {
 
@@ -10,8 +13,17 @@ public class SongFileNameHelper {
     };
 
     public static String generateSongFileName(SongDataStruct song){
-        String fileName = String.format("%s_%s_%s.mp3", song.getArtist().getName(),
-                song.getAlbum().getTitle(), song.getSongName())
+        final AlbumDataStruct album = song.getAlbum();
+        return generateFileName(song.getArtist().getName(), album.getTitle(), song.getSongName(), album.getId());
+    }
+
+    public static String generateSongFileName(String artistName, String albumName, String songName, int albumId){
+        return generateFileName(artistName, albumName, songName, albumId);
+    }
+
+    private static String generateFileName(String artistName, String albumName, String songName, int albumId){
+        String fileName = String.format(Locale.getDefault(), "%s_%s_%s_%d.mp3", artistName,
+                albumName, songName, albumId)
                 .replace(' ', '_');
         for (String c : charactersToBeRemoved){
             fileName = fileName.replace(c, "");
